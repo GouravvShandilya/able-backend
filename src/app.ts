0250587c23d62ee5d7835495bf6f3d63e34859b6
Gouravshandilya69 <i.gouravshandilya@gmail.com>
2024-05-08 19:56:11 +0530

    ASgasgsa

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const app = express();
 var cors = require("cors");
 
 const corsOptions = {
-  origin: 'https://able-frontend.vercel.app/', // Allow requests from all origins
+  origin: '*', // Allow requests from all origins
   credentials: true // Allow credentials (e.g., cookies)
 };
 
